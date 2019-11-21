2251dd7b2148f67b1d3f9f72ab58055e8915856c
yohei <yohei@mail.jp>
2019-11-21 10:25:51 +0900

    簡易生命表

--- capture/data_import.py
+++ capture/data_import.py
@@ -16,17 +16,17 @@ from operator import itemgetter
 # 	print('中央値：'+str(rv.median(row)))
 # 	print('最頻値：'+str(rv.mode(row)))
 
-text = ''
-filename = '../data/全国消費実態調査平成21年全国消費実態調査全国貯蓄負債編.csv'
-with open(filename) as f:
-	r = csv.reader(f, delimiter=',')
-	rows = [[ll for ll in l] for l in r]
-	print(rows[0])
-	print(rows[1])
-	datarow = [1,5,10,11,13]
-	data_col = [k for k in range(9,50,2)]
-	for i in datarow:
-		text += itemgetter(*data_col)(rows[i])
+# text = ''
+# filename = '../data/全国消費実態調査平成21年全国消費実態調査全国貯蓄負債編.csv'
+# with open(filename) as f:
+# 	r = csv.reader(f, delimiter=',')
+# 	rows = [[ll for ll in l] for l in r]
+# 	print(rows[0])
+# 	print(rows[1])
+# 	datarow = [1,5,10,11,13]
+# 	data_col = [k for k in range(9,50,2)]
+# 	for i in datarow:
+# 		text += itemgetter(*data_col)(rows[i])
 
 # filename = 'sampledata20191017.csv'
 # with open(filename,'w')  as f2:
@@ -40,20 +40,20 @@ with open(filename) as f:
 	# print('中央値：'+str(rv.median(row)))
 	# print('最頻値：'+str(rv.mode(row)))
 
-# arg1 = sys.argv[1]
+arg1 = sys.argv[1]
 
 #######ALL DATA########
-# with open(arg1) as f:
-# 	print (f.read())
-	
+with open(arg1) as f:
+	print (f.read())
+
 
 ######ALL DATA per 1########
 # with open(arg1) as f:
 # 	r = csv.reader(f, delimiter=',')
 # 	for row in r:
 		# print('列数:' + str(len(row)))
-		# print (fn.average(int(row))) 
-		# print (row) 
+		# print (fn.average(int(row)))
+		# print (row)
 
 ######ALL DATA per 1 in array########
 # with open(arg1) as f:
@@ -67,8 +67,8 @@ with open(filename) as f:
 	# 	print(l[1][i] + ' ' + l[5][i] + ' ' + l[10][i] + ' ' + l[11][i] + ' ' + l[13][i])
 	# for ll in l:
 	# 	ll.sort()
-	# 	print (fn.average(ll)) 
-	# 	print (fn.median(ll)) 
+	# 	print (fn.average(ll))
+	# 	print (fn.median(ll))
 # 5	世帯人員（人）
 # 11	年間収入（千円）
 # 10	世帯主の年齢（歳）
